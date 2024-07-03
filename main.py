dff0c5cbd866f23469a635773ca311e93db0be86
johhnMunene <Edwardsjohnny64@gmail.com>
2024-07-03 18:23:38 +0300

    add registration for the KEEPERAPI

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
+# main.py
+
 from fastapi import FastAPI, HTTPException
 from tortoise.contrib.fastapi import register_tortoise
 from tortoise.contrib.pydantic import pydantic_model_creator
 from models import User, Keeper, Product
 from auth import get_hashed_password
 
+# Signals
+from tortoise.signals import post_save
+from typing import List, Optional, Type
+from tortoise import BaseDBAsyncClient
+
 app = FastAPI()
+
+@post_save(User)
+async def create_business(
+    sender: "Type[User]",
+    instance: User,
+    created: bool,
+    using_db: "Optional[BaseDBAsyncClient]",
+    update_fields: List[str]
+) -> None:
+    if created:
+        # business_obj = await Business.create(
+        #     business_name=instance.username,
+        #     owner=instance
+        # )
+        # await business_pydantic.from_tortoise_orm(business_obj)
+        # Send email here
+        pass
 
 # Pydantic models for serialization and validation
 User_Pydantic = pydantic_model_creator(User, name="User", exclude=("is_verified",))
@@ -19,13 +43,17 @@ async def root():
 @app.post("/registrations")
 async def register_user(user: UserIn_Pydantic):
     user_info = user.dict(exclude_unset=True)
-    user_info["password"] = get_hashed_password(user_info["password"])  # Assuming get_hashed_password is correctly implemented
+    user_info["password"] = get_hashed_password(user_info["password"])
     user_obj = await User.create(**user_info)
-    return await UserOut_Pydantic.from_tortoise_orm(user_obj)
+    new_user = await User_Pydantic.from_tortoise_orm(user_obj)
+    return {
+        "status": "ok",
+        "data": f"Hello {new_user.username}, thanks for using KEEPERAPI. Check your email to confirm your registration."
+    }
 
 register_tortoise(
     app,
-    db_url='mysql://username:password@localhost:3306/mydatabase',
+    db_url="sqlite://db.sqlite3",  # Use your actual database URL
     modules={"models": ["models"]},
     generate_schemas=True,
     add_exception_handlers=True
