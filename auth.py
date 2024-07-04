8733120233e48d85da7c9c8cb647a547d9ae1a15
johhnMunene <Edwardsjohnny64@gmail.com>
2024-07-04 15:50:54 +0300

    adding tockets for email and html

--- auth.py
+++ auth.py
@@ -1,7 +1,19 @@
 from passlib.context import CryptContext
-
+import jwt
+from dotenv import dotenv_values
+from .models import User
+fro
+config credentials = dotenv_values("env")
 
 pass_context = CryptContext(schemes = ['bcrypt'],deprecated='auto')
 
 def get_hashed_password(password):
     return pass_context.hash(password)
+
+async def very_token(token:str):
+    try:
+        """consists of user id"""
+        payload = jwt.decode(token,config_credential['SECRET'])
+        user = await User.get(id =payload.get("id"))# decode theuser  token
+    
+
