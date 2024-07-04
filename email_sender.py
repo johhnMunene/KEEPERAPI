a0044020ec9f4d752bd96393b9343f23d43ed610
johhnMunene <Edwardsjohnny64@gmail.com>
2024-07-04 15:37:59 +0300

    adding tockets for email and html

--- email_sender.py
+++ email_sender.py
@@ -1,18 +1,44 @@
 from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
-
-
+from dotenv import dontenv_values
+from pydantic import BaseModel,EmailStr
+config_credentials = dotenv_values(".env")
+from typing import List
+from .models import User
+import jwt
 conf = ConnectionConfig(
-    MAIL_USERNAME="",
-    MAIL_PASSWORD="",
-    MAIL_FROM="your_email@example.com",
-    MAIL_PORT=465,
-    MAIL_SERVER="smg.gmail.com",
-    MAIL_STARTTLS=False,
-    MAIL_SSL_TLS=True,
+    MAIL_USERNAME=config_credentials["EMAIL"],
+    MAIL_PASSWORD=config_credentials["PASSWORD"],
+    MAIL_FROM=config_credentials["EMAIL"],
+    MAIL_PORT=587,
+    MAIL_SERVER="smpt.gmail.com",
+    MAIL_SSL =False,
+    MAIL_TLS=True,
     USE_CREDENTIALS=True,
     VALIDATE_CERTS=True
 )
 
 html = """
+<h1> Account Verification</h1>
 <p>Thanks for using KeeperAPI</p>
+<a href = "http://localhost:8000/verification/?token={token}> Verify your email</a>
+
+
 """
+
+class EmailSchema(Basemodel):
+    email : List{EmailStr]
+
+async def Email_sender(email:EmailSchema,instance:User)
+token_data = {
+        "id" : instance.id,
+        "username": instance.username
+        }
+token = jwt.encode(token_data,config_credentials["SECRET"])
+   message = MessageSchema(
+        subject= "KeeperAPI Account Verification Email",
+        recipients=email#List emails,
+        body=html,
+        subtype=html
+        )
+   fm =FastMail(conf)
+   awaitfm.send_message(message=message)
